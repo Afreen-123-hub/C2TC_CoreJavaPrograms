fa956bba3d4f67f694b53d1e999dfc134165e20d
Lenovo <Lenovo@LAPTOP-I8LOP7IJ>
2025-12-16 21:19:17 +0530

    commit 1

---
+++ Java_Code/src/com/tnsif/basics/ForLoopEg.java
@@ -0,0 +1,15 @@
+package com.tnsif.basics;
+
+public class ForLoopEg {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=1000; i++)
+		{
+			System.out.println("Value of i:");
+			System.out.println(i);
+
+		}
+
+	}
+
+}
