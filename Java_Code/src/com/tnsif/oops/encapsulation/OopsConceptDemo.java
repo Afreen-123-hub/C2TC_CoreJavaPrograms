a139d81b58077fb3ec8e5a942ae184f1ddb5c895
Lenovo <Lenovo@LAPTOP-I8LOP7IJ>
2025-12-18 10:55:52 +0530

    commit 3

---
+++ Java_Code/src/com/tnsif/oops/encapsulation/OopsConceptDemo.java
@@ -0,0 +1,34 @@
+package com.tnsif.oops.encapsulation;
+
+public class OopsConceptDemo {
+          //var declaration /data members
+		private int serialNum;       //int,string,boolean,float
+		private String name;         //private(accesable within the class),public,protected
+		private int age;
+		
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+		//getters and setters method(built in method)
+		public int getSerialNum() {      //get means get input 
+			return serialNum;
+		}
+		public void setSerialNum(int serialNum) {     //set means place in correct position
+			this.serialNum=serialNum;	
+			}      //this is a keyword refers to current obj
+
+             @Override
+             public String toString() {
+            	 return "OopsConceptDemo [Serial number=" +serialNum+"," +"name =" +name+", age=" +age+"]";
+             }
+             }
+
